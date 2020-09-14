df18f6fcb960fe85aec3a51a14297e848d17e3fe
QuadCrew4 <quadcrewz@gmail.com>
2020-09-14 15:48:26 +0530

    Commit by Saiteja

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div style="text-align :center" class="content">
+  <h1>Welcome to {{title}}</h1>
+  <h2>Pushing into Github Angular Repository by Saiteja</h2>
+  </div>
+ `,
+ styles: [
+   `h1,h2{
+     color:red,
+     font-family:verdana;
+   }`
+ ]
+})
+export class AppComponent {
+  title = 'practice-app';
+}
